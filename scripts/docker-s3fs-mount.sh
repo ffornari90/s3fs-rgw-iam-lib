9c448031b96d8533333541a6aad046f092f0ac6f
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-04-12 16:31:56 +0200

    Fixed non-working container deployment in background

--- scripts/docker-s3fs-mount.sh
+++ scripts/docker-s3fs-mount.sh
@@ -2,8 +2,8 @@
 ROOTDIR=$(git rev-parse --show-toplevel)
 FILE="${ROOTDIR}/rgw-sts-profile"
 if [ -f "$FILE" ]; then
-  docker run --name=s3fs --rm \
-           --net=host -d \
+  docker run --name=s3fs-client$1 --rm \
+           --net=host -ti \
            --device /dev/fuse \
            --cap-add SYS_ADMIN \
            --privileged \
