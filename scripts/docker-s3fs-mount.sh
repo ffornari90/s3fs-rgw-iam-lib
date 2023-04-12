408956db5eb845c5c0c319a6b98d629d26761e1c
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-04-12 16:17:13 +0200

    Added customized command for container startup

--- scripts/docker-s3fs-mount.sh
+++ scripts/docker-s3fs-mount.sh
@@ -3,10 +3,18 @@ ROOTDIR=$(git rev-parse --show-toplevel)
 FILE="${ROOTDIR}/rgw-sts-profile"
 if [ -f "$FILE" ]; then
   docker run --name=s3fs --rm \
-           --net=host -ti \
+           --net=host -d \
            --device /dev/fuse \
            --cap-add SYS_ADMIN \
            --privileged \
            --env-file $FILE \
-           ffornari/s3fs-rgw-iam
+           ffornari/s3fs-rgw-iam \
+           sh -c \
+           "s3fs \$BUCKET_NAME \$HOME/mnt/rgw \
+           -o use_cache=/tmp \
+           -o use_path_request_style \
+           -o url=\$ENDPOINT_URL \
+           -o endpoint=\$REGION_NAME \
+           -o credlib=librgw-sts.so \
+           -o credlib_opts=Info -f"
 fi
