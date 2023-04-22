06ce91d8bfe6c05506003ee8bfc7d85687919cfc
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-04-22 14:38:35 +0200

    Updated docker image and script

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t ffornari/s3fs-rgw-dependencies -f dependencies.Dockerfile .
-docker build -t ffornari/s3fs-rgw-iam -f Dockerfile .
+docker build -t ffornari/s3fs-rgw-iam:auth-code -f Dockerfile .
--- scripts/docker-s3fs-mount.sh
+++ scripts/docker-s3fs-mount.sh
@@ -3,12 +3,12 @@ ROOTDIR=$(git rev-parse --show-toplevel)
 FILE="${ROOTDIR}/rgw-sts-profile"
 if [ -f "$FILE" ]; then
   docker run --name=s3fs-client$1 --rm \
-           --net=host -ti \
+           --net=host -d \
            --device /dev/fuse \
            --cap-add SYS_ADMIN \
            --privileged \
            --env-file $FILE \
-           ffornari/s3fs-rgw-iam \
+           ffornari/s3fs-rgw-iam:auth-code \
            sh -c \
            "s3fs \$BUCKET_NAME \$HOME/mnt/rgw \
            -o use_cache=/tmp \
