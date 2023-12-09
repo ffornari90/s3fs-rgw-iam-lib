6a238fe19dd0223be653fbe07b56b1e974641ef4
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-12-09 16:47:25 +0100

    Updated mount script

--- scripts/docker-s3fs-mount.sh
+++ scripts/docker-s3fs-mount.sh
@@ -2,14 +2,14 @@
 ROOTDIR=$(git rev-parse --show-toplevel)
 FILE="${ROOTDIR}/profiles/rgw-sts-profile-$1"
 if [ -f "$FILE" ]; then
-  docker run --name=s3fs-client$1 --rm \
+  docker run --name=s3fs-client$1 \
            --net=host -d \
            --device /dev/fuse \
            --cap-add SYS_ADMIN \
            --privileged \
            --env-file $FILE \
-           -v "${ROOTDIR}/certs/certs-client$1/private.key":/home/docker/private.key \
-           -v "${ROOTDIR}/certs/certs-client$1/public.crt":/home/docker/public.crt \
+           -v "${ROOTDIR}/certs/client$1/private.key":/home/docker/private.key \
+           -v "${ROOTDIR}/certs/client$1/public.crt":/home/docker/public.crt \
            ffornari/s3fs-rgw-iam:auth-code-ubuntu \
            sh -c \
            "s3fs \$BUCKET_NAME \$HOME/mnt/rgw \
