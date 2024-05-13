a9d54c22068caea80108876acebe8d56ea97904c
ffornari90 <federico.fornari@cnaf.infn.it>
2024-05-13 11:19:13 +0200

    Debug curl response

--- src/getOIDCRefreshToken.cpp
+++ src/getOIDCRefreshToken.cpp
@@ -30,6 +30,7 @@ Aws::String getOIDCRefreshToken(const std::string &IAMHost, const std::string &c
               curl_easy_strerror(res));
     curl_easy_cleanup(curl);
   }
+  fprintf(stderr, "curl_response: %s\n", readBufferDiscovery);
   if (nlohmann::json::accept(readBufferDiscovery)) {
     nlohmann::json data = nlohmann::json::parse(readBufferDiscovery);
     if (data.contains("device_authorization_endpoint")) {
